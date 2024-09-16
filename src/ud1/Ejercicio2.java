7ef679602c14342a48567edf54ca72c0473ce2c7
vagosu <vagos11234@gmail.com>
2024-09-16 20:48:01 +0200

    aaaaa

--- src/ud1/Ejercicio2.java
+++ src/ud1/Ejercicio2.java
@@ -1,10 +1,34 @@
 package ud1;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
 public class Ejercicio2 {
 
     public static void main(String[] args) {
         // 1
+        Path path1 = Path.of("ej2");
+        Path path2 = Path.of("files");
 
+        directoryCompare(path1, path2);
+
+
+    }
+
+    // 1
+    public static Stream<Path> directoryCompare(Path path1, Path path2){
+        Stream<Path> diffs;
+        try {
+            Stream<Path> stream1 = Files.list(path1);
+            Stream<Path> stream2 = Files.list(path2);
+
+        } catch (IOException e) {
+            System.err.println("Error....");
+        }
+
+        return diffs;
     }
 
 }
